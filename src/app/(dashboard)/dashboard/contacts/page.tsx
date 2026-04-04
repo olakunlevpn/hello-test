f0cc9a0ef9f2e4dc5d77d4523b3f3497b50ef95b
Olakunlevpn <olakunlevpn@live.com>
2026-04-04 19:38:16 +0100

    update contacts pagination options to 10, 20, 50, 100, All

--- src/app/(dashboard)/dashboard/contacts/page.tsx
+++ src/app/(dashboard)/dashboard/contacts/page.tsx
@@ -268,10 +268,10 @@ export default function ContactsPage() {
                     <SelectValue />
                   </SelectTrigger>
                   <SelectContent>
+                    <SelectItem value="10">10</SelectItem>
                     <SelectItem value="20">20</SelectItem>
                     <SelectItem value="50">50</SelectItem>
                     <SelectItem value="100">100</SelectItem>
-                    <SelectItem value="200">200</SelectItem>
                     <SelectItem value="all">{t("filterAll")}</SelectItem>
                   </SelectContent>
                 </Select>
